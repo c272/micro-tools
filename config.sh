f38adfaa359a2bb93a7c1fc5d1315d8c8d125f20
c272 <larry@c272.org>
2022-08-25 12:04:56 +0100

    Add initial microbuild script.

---
+++ config.sh
@@ -0,0 +1,8 @@
+# Location of the micro:bit v2 SDK (as cloned from microbit-v2-samples).
+# export MICROBIT_SDK_DIRECTORY=
+
+# Default build directory.
+BUILD_DIRECTORY="."
+
+# Default output directory.
+BUILD_OUTPUT_DIRECTORY="."
---
+++ microbuild.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+set -e
+
+# A utility script to make building micro:bit projects outside of the micro:bit v2 samples repository
+# a little bit easier.
+
+# Colour code setup.
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# Print version information.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+COMMIT=$(cd $SCRIPT_DIR && git rev-parse HEAD)
+echo -e "${CYAN}microbuild v0.1, (c) C272 2022${NC}"
+echo -e "${CYAN}commit: ${NC}"
+
+# Run the configuration script.
+if [ -f "config.sh" ]; then
+    echo "Running configuration script..."
+    source ./config.sh
+fi
+
+# Parse arguments passed in directly.
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    KEY_LENGTH=${#KEY}
+    VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+    export "$KEY"="$VALUE"
+done
+
+# If "MICROBIT_SDK_DIRECTORY" does not exist, error out.
+if [[ ! -n "$MICROBIT_SDK_DIRECTORY" ]]; then
+  echo -e "${RED}The micro:bit v2 SDK directory was not specified. Either specify this in \`config.sh\`, or via the MICROBIT_SDK_DIRECTORY=... parameter.${NC}"
+  exit -1
+fi
+
+# If "BUILD_DIRECTORY" does not exist, error out.
+if [[ ! -n "$BUILD_DIRECTORY" ]]; then
+  echo -e "${RED}The directory to build was not specified. Either specify this in \`config.sh\`, or via the BUILD_DIRECTORY=... parameter.${NC}"
+  exit -1
+fi
+
+# Trim all paths of trailing slashes.
+MICROBIT_SDK_DIRECTORY=$(echo $MICROBIT_SDK_DIRECTORY | sed 's:/*$::')
+BUILD_DIRECTORY=$(echo $BUILD_DIRECTORY | sed 's:/*$::')
+
+# Ensure that the directories exist.
+if [[ ! -d "$MICROBIT_SDK_DIRECTORY" ]]; then
+  echo -e "${RED}The micro:bit v2 SDK directory provided ('$MICROBIT_SDK_DIRECTORY') does not exist.${NC}"
+  exit -1
+fi
+if [[ ! -d "$BUILD_DIRECTORY" ]]; then
+  echo -e "${RED}The micro:bit v2 SDK directory provided ('$MICROBIT_SDK_DIRECTORY') does not exist.${NC}"
+  exit -1
+fi
+
+# Ensure that the micro:bit SDK directory provided is *actually* an SDK directory.
+if [[ ! -f "$MICROBIT_SDK_DIRECTORY/build.py" ]]; then
+  echo -e "${RED}No 'build.py' file was found in provided SDK directory ('$MICROBIT_SDK_DIRECTORY'). Are you sure this is a valid micro:bit SDK directory?${NC}"
+  exit -1
+fi
+
+# Get absolute path of build directory, microbit SDK directory.
+MICROBIT_SDK_DIRECTORY=$(realpath $MICROBIT_SDK_DIRECTORY)
+BUILD_DIR_ABSOLUTE=$(realpath $BUILD_DIRECTORY)
+
+# Ensure the microbit SDK source folder/source folder symlink does not exist.
+TARGET_SOURCE_FOLDER="$MICROBIT_SDK_DIRECTORY/source"
+if [[ -L $TARGET_SOURCE_FOLDER && -d $TARGET_SOURCE_FOLDER ]]
+then
+    echo "Cleaning up existing source folder symlink at $TARGET_SOURCE_FOLDER..."
+    rm "$MICROBIT_SDK_DIRECTORY/source"
+elif [[ -d $TARGET_SOURCE_FOLDER ]]
+then
+    echo "Cleaning up existing source folder at $TARGET_SOURCE_FOLDER..." 
+    rm -r "$MICROBIT_SDK_DIRECTORY/source"
+fi
+
+# Create a symlink from the source folder to the build folder.
+echo -e "${CYAN}Symlinking build directory to SDK source directory...${NC}"
+ln -s $BUILD_DIR_ABSOLUTE $TARGET_SOURCE_FOLDER
+
+# Begin the build.
+echo -e "${CYAN}Beginning build...${NC}"
+ORIGINAL_PWD=$PWD
+cd $MICROBIT_SDK_DIRECTORY
+python3 build.py
+cd $ORIGINAL_PWD
+
+# Copy result of build to output folder, if specified.
+if [[ -n "$BUILD_OUTPUT_DIRECTORY" ]]; then
+
+    echo -e "${CYAN}Copying build output to output directory...${NC}"
+
+    # Ensure it exists.
+    BUILD_OUTPUT_DIR_ABSOLUTE=$(realpath $BUILD_OUTPUT_DIRECTORY)
+    mkdir -p $BUILD_OUTPUT_DIR_ABSOLUTE
+
+    # Copy the micro:bit hex file over.
+    cp -u $MICROBIT_SDK_DIRECTORY/MICROBIT.hex $BUILD_OUTPUT_DIR_ABSOLUTE
+fi
+
+echo -e "${GREEN}Build complete!${NC}"
